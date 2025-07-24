de7bd48c05989148a512dab9aee3ca55820e5f5b
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-07-24 20:59:52 +1000

    First release

---
+++ tests/test_parse.py
@@ -0,0 +1,20 @@
+from jj_pre_push.bookmark_updates import BookmarkUpdate, parse_git_push_dry_run
+
+
+def test_parse_git_push_dry_run():
+    output = """\
+Changes to push to origin:
+  Move forward bookmark main from d964e724c76e to a81d749233ff
+  Add bookmark painstaking to 591f7e9aae85
+  Move sideways bookmark sideways from 9c712e75a982 to 23f89ce4b31b
+  Move backward bookmark backward from d964e724c76e to 561998a40ada
+  Delete bookmark deleted from 9c712e75a982
+Dry-run requested, not pushing.
+"""
+    assert parse_git_push_dry_run(output) == {
+        BookmarkUpdate("main", "move_forward", "d964e724c76e", "a81d749233ff"),
+        BookmarkUpdate("painstaking", "add", None, "591f7e9aae85"),
+        BookmarkUpdate("sideways", "move_sideways", "9c712e75a982", "23f89ce4b31b"),
+        BookmarkUpdate("backward", "move_backward", "d964e724c76e", "561998a40ada"),
+        BookmarkUpdate("deleted", "delete", "9c712e75a982"),
+    }

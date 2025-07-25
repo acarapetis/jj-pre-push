c52009114aa814b85a00a9cf08e7dfec99191bd5
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-07-26 03:10:55 +1000

    check-and-push

--- src/jj_pre_push/cli.py
+++ src/jj_pre_push/cli.py
@@ -6,8 +6,10 @@ app = typer.Typer()
 
 
 @app.command()
-def check_and_push(
-    remote: str | None = None, bookmark: str | None = None, all: bool = False
+def check(
+    remote: str | None = None,
+    bookmark: str | None = None,
+    all: bool = False,
 ):
     if remote is None:
         remote = jj.default_remote()
@@ -16,6 +18,7 @@ def check_and_push(
         keep = jj.default_bookmarks_to_push(remote)
         bookmarks = [b for b in bookmarks if b.name in keep]
     for b in bookmarks:
+        print(f"Checking {b}")
         with jj.checkout(b.local_commit_id):
             if (
                 subprocess.run(
@@ -30,10 +33,21 @@ def check_and_push(
                 ).returncode
                 != 0
             ):
-                print(
-                    f"pre-commit checks failed for bookmark {b.name} ({b.remote_commit_id}..{b.local_commit_id})"
-                )
+                print(f"pre-commit checks failed for bookmark {b}")
                 raise typer.Exit(1)
+
+
+@app.command()
+def check_and_push(
+    remote: str | None = None,
+    bookmark: str | None = None,
+    all: bool = False,
+):
+    check(remote, bookmark, all)
+    try:
+        jj.git_push(remote, bookmark, all)
+    except subprocess.CalledProcessError as e:
+        raise typer.Exit(e.returncode)
 
 
 def main():

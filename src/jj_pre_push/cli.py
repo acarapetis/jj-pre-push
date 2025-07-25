d7fc8b4a995e36bdc23c5e58096f90e67d68ed3b
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-07-26 03:03:30 +1000

    run pre-commit

--- src/jj_pre_push/cli.py
+++ src/jj_pre_push/cli.py
@@ -18,19 +18,16 @@ def check_and_push(
     for b in bookmarks:
         print(f"Would push {b.name}: {b.remote_commit_id}..{b.local_commit_id}")
         with jj.checkout(b.local_commit_id):
-            subprocess.check_call(["jj", "status"])
-            subprocess.check_call(["jj", "log"])
-            subprocess.check_call(["git", "status"])
-            # subprocess.check_call(
-            #     [
-            #         "pre-commit",
-            #         "run",
-            #         "--from-ref",
-            #         b.remote_commit_id,
-            #         "--to-ref",
-            #         b.local_commit_id,
-            #     ]
-            # )
+            subprocess.check_call(
+                [
+                    "pre-commit",
+                    "run",
+                    "--from-ref",
+                    b.remote_commit_id,
+                    "--to-ref",
+                    b.local_commit_id,
+                ]
+            )
 
 
 def main():

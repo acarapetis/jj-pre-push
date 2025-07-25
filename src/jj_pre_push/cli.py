18b5e5aa2635080ecfd75f0ecb46e2836c1e95cd
Anthony Carapetis <anthony.carapetis@gmail.com>
2025-07-26 02:37:40 +1000

    actually do stuff

--- src/jj_pre_push/cli.py
+++ src/jj_pre_push/cli.py
@@ -1,6 +1,34 @@
-import sys
+import subprocess
+from jj_pre_push import jj
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def check_and_push(
+    remote: str | None = None, bookmark: str | None = None, all: bool = False
+):
+    if remote is None:
+        remote = jj.default_remote()
+    bookmarks = jj.pushable_bookmarks(remote, bookmark=bookmark, all=all)
+    if not (bookmark or all):
+        keep = jj.default_bookmarks_to_push(remote)
+        bookmarks = [b for b in bookmarks if b.name in keep]
+    for b in bookmarks:
+        print(f"Would push {b.name}: {b.remote_commit_id}..{b.local_commit_id}")
+        jj.jj_raw(["new", b.local_commit_id])
+        subprocess.check_call(
+            [
+                "pre-commit",
+                "run",
+                "--from-ref",
+                b.remote_commit_id,
+                "--to-ref",
+                b.local_commit_id,
+            ]
+        )
 
 
 def main():
-    print(repr(sys.executable))
-    print(sys.argv)
+    app()
